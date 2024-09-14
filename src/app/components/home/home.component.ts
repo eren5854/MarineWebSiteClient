507ea1756980dde42cb98a7685abffa2567a2f72
eren5854 <ihsanerendelibas@hotmail.com>
2024-09-14 04:54:26 +0300

    .

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,38 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import * as AOS from 'aos';
-import { LayoutComponent } from '../layout/layout.component';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
   isScreenSizeUnder768px = false;
 
-  title: string = "Marqex"
-  backgroundImage: string = 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("/assets/yacht-2.jpg")'
-
-  constructor(
-    private layout: LayoutComponent
-  ) {
+  constructor() {
     this.checkWindowSize();
-    this.layout.setTitle(this.title, this.backgroundImage);
-    // AOS.init({
-    //   duration: 2000,
-    //   delay: 200,
-    //   once: false
-    // }); // AOS'u başlat
-  }
-
-  ngOnInit() {
-
   }
 
   @HostListener('window:resize', ['$event'])
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { IonIcon } from "@ionic/angular/standalone";
-import * as AOS from 'aos';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [IonIcon, RouterOutlet, FormsModule, CommonModule, RouterLink],
+  imports: [IonIcon, RouterOutlet, FormsModule, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
@@ -17,41 +15,15 @@ export class LayoutComponent {
   isMenuOpen = false;
   isScreenSizeUnder768px = false;
   isNavigateOpen = false;
-
-  title:string = "Marteq"
-  backgroundImage:string = 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("/assets/yacht-2.jpg")'
-  
   private scrollHandler: () => void;
 
-  constructor(
-    private router: Router
-  ) {
+  constructor() {
     this.scrollHandler = this.scrollActive.bind(this);
     this.checkWindowSize();
-    AOS.init({
-      duration: 2000,
-      delay:200,
-      once:true
-    }); // AOS'u başlat
-    
-  }
-
-  setTitle(newTitle: string, newBackground: string) {
-    this.title = newTitle;
-    this.backgroundImage = newBackground;
-  }
-
-  reload(){
-    location.reload();
   }
 
   ngOnInit(): void {
-    
     window.addEventListener('scroll', this.scrollHandler);
-  }
-
-  startAos(){
-    AOS.refreshHard();
   }
 
   ngOnDestroy(): void {
@@ -87,7 +59,7 @@ export class LayoutComponent {
   
     sections.forEach(current => {
       const sectionHeight = (current as HTMLElement).offsetHeight;
-      const sectionTop = (current as HTMLElement).offsetTop - 700;
+      const sectionTop = (current as HTMLElement).offsetTop - 700; // Navigasyon yüksekliğinize göre ayarlayın
       const sectionId = (current as HTMLElement).getAttribute('id') || '';
   
       const link = document.querySelector(`.nav__menu a[href*="${sectionId}"]`);
